4277022c6ce63145a5c0756e37652653ea0898ca
AnikHassan111 <miaanik4@gmail.com>
2023-11-29 00:26:58 +0600

    add owner product update function

--- src/Pages/Product/ProductUpdate/ProductUpdate.jsx
+++ src/Pages/Product/ProductUpdate/ProductUpdate.jsx
@@ -4,10 +4,10 @@ import { TagsInput } from "react-tag-input-component";
 import Sesctiontitle from "../../../ComPonent/SectionTitle/Sesctiontitle";
 import useAxiosSecureApi from "../../../Hooks/axiosSecureapi/useAxiosSecureApi";
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ProductUpdate = () => {
-  const [selected, setSelected] = useState([]);
   const {
     register,
     handleSubmit,
@@ -17,16 +17,41 @@ const ProductUpdate = () => {
 
   const axiosSecure = useAxiosSecureApi();
   const { id } = useParams();
-  const { data: ownerproduct } = useQuery({
+  const { data: ownerproduct = {}, refetch } = useQuery({
     queryKey: ["updateOwnerdata"],
     queryFn: async () => {
-      const res = await axiosSecure.get(`/ownerProductUpdate/${id}`);
+      const res = await axiosSecure.get(`/ownerProductget/${id}`);
       return res.data;
     },
   });
-  const { img, name, tags, description, externalLink } = ownerproduct;
+  const { img, name, description, externalLink } = ownerproduct;
+  const navigate = useNavigate();
 
-  const onSubmit = (data) => {};
+  const onSubmit = (data) => {
+    const updateProduct = {
+      img: data?.Image,
+      name: data?.name,
+      description: data?.Description,
+      externalLink: data?.ExternalLinks,
+    };
+    console.log(updateProduct);
+    axiosSecure
+      .patch(`/ownerProductUpdate/${id}`, updateProduct)
+      .then((res) => {
+        if (res.data.modifiedCount > 0) {
+          Swal.fire({
+            title: " Success",
+            text: "Your Product Add SuccessFully ",
+            icon: "success",
+          });
+          navigate("/dashboard/myProduct");
+          refetch();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <div className="p-20">
@@ -40,6 +65,7 @@ const ProductUpdate = () => {
               <input
                 id="username"
                 placeholder="Product Name"
+                defaultValue={name}
                 type="text"
                 name="name"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
@@ -58,6 +84,7 @@ const ProductUpdate = () => {
               </label>
               <input
                 id="emailAddress"
+                defaultValue={img}
                 placeholder=" Product Image"
                 type="text"
                 name="Image"
@@ -76,8 +103,9 @@ const ProductUpdate = () => {
                 Product Description
               </label>
               <input
-                id="photo"
+                id="description"
                 placeholder="Product Description"
+                defaultValue={description}
                 name="Description"
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
@@ -92,22 +120,12 @@ const ProductUpdate = () => {
 
             <div>
               <label className="text-gray-700 dark:text-gray-200">
-                Product tags
-              </label>
-              <TagsInput
-                value={selected}
-                onChange={setSelected}
-                name="fruits"
-                placeHolder="enter fruits"
-              />
-            </div>
-            <div>
-              <label className="text-gray-700 dark:text-gray-200">
                 External Links
               </label>
               <input
                 id="photo"
                 name="ExternalLinks"
+                defaultValue={externalLink}
                 placeholder="External Links"
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
