18655a74ba9ecfae229d47689d9208b1bd3ca874
AnikHassan111 <miaanik4@gmail.com>
2023-11-30 02:17:02 +0600

    add product search functionly

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,31 +1,51 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProdcutCard from "./ProductCard";
 import useAxiosPublicApi from "../../Hooks/axiosPublicapi/useAxiosPublicApi";
 
 const Product = () => {
   const axiosPublic = useAxiosPublicApi();
-  const { data: prodcut = [], refetch } = useQuery({
-    queryKey: ["product"],
+  const [prodcut, setprodcut] = useState(null);
+  const [input, setInput] = useState("");
+  const { data: mainProduct = [], refetch } = useQuery({
+    queryKey: ["mainProduct"],
     queryFn: async () => {
       const res = await axiosPublic("/product");
       return res.data;
     },
   });
+  useEffect(() => {
+    setprodcut(mainProduct);
+  }, [mainProduct]);
+  const handleSearch = () => {
+    axiosPublic
+      .get(`/searchProduct/${input}`)
+      .then((res) => {
+        setprodcut(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="pt-20 max-w-7xl mx-auto ">
       <div className="w-fit mx-auto my-4">
         <input
+          onInput={(e) => setInput(e.target.value)}
           type="text"
           className="bg-black py-2 px-4 rounded-lg text-white mr-2 lg:w-96 w-42"
           placeholder="search here"
         />
-        <button className="py-2 px-4 bg-slate-600 text-white rounded-lg ">
+        <button
+          onClick={handleSearch}
+          className="py-2 px-4 bg-slate-600 text-white rounded-lg "
+        >
           Search
         </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-2 gap-3 ">
-        {prodcut.map((item) => (
+        {prodcut?.map((item) => (
           <ProdcutCard
             key={item._id}
             product={item}
